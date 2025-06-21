26cdbdeab488fc510167529903ce703a33e05b28
natriumkr <wookuki1004@gmail.com>
2025-06-21 06:37:44 +0000

    modified:   components/js/index.js

--- components/js/index.js
+++ components/js/index.js
@@ -1,16 +1,16 @@
 function changeLanguage(lang) {
     if (lang === "ko") {
-      window.location.href = "pages/ko/index.html";
+      window.location.href = "../../pages/mainpage/ko/index.html";
     } else if (lang === "en") {
-      window.location.href = "../../pages/mainpage/index.html";
+      window.location.href = "../../pages/mainpage/en/index.html";
     } else if (lang === "cn") {
-      window.location.href = "../../pages/mainpage/index.html";
+      window.location.href = "../../pages/mainpage/cn/index.html";
     } else if (lang === "jp") {
-      window.location.href = "../../pages/mainpage/index.html";
+      window.location.href = "../../pages/mainpage/jp/index.html";
     } else if (lang === "ru") {
-      window.location.href = "../../pages/mainpage/index.html";
+      window.location.href = "../../pages/mainpage/ru/index.html";
     } else if (lang === "ar") {
-      window.location.href = "../../pages/mainpage/index.html";
+      window.location.href = "../../pages/mainpage/ar/index.html";
     } else {
       window.location.href = "index.html";
     }
