01c8cc9a4be58ab5a310d46a32f04d82936cfdba
natriumkr <wookuki1004@gmail.com>
2025-06-21 01:54:22 +0000

    new file:   index.html 	new file:   lang-data.js 	new file:   script.js 	new file:   style.css

---
+++ lang-data.js
@@ -0,0 +1,16 @@
+const translations = {
+  ko: {
+    title: "트립버디 | 당신의 여행계획을 제작하세요! ",
+    events: "이벤트",
+    header1: "여행계획 제작",
+    header2: "로그인",
+    search_section1: "당신의 여행계획을 제작하세요!",
+  },
+  en: {
+    title: "TripBuddy | Create your own Plan! ",
+    events: "Events",
+    header1: "Create Your Travel Plan",
+    header2: "Sign In",
+    search_section1: "Create your Travel Plan!",
+  },
+};
